9874a8d6408210a92cf47f37c474a19cc2cb3692
Bojana <bojana.karakacev@gmail.com>
2017-12-06 19:44:18 +0100

    Convert tabs to spaces

--- src/application.js
+++ src/application.js
@@ -3,51 +3,44 @@ import swal from 'sweetalert2';
 import randomColor from 'randomcolor';
 
 $(document).ready(function() {
-	$('#new').on('click', function() {
-		$('form').toggle();
-		$('.grid table').hide();
-	});
+  $('#new').on('click', function() {
+    $('form').toggle();
+    $('.grid table').hide();
+  });
 
-	$('#submit').on('click', function(event) {
-		event.preventDefault();
-		let number = $('input').val();
+  $('#submit').on('click', function(event) {
+    event.preventDefault();
+    let number = $('input').val();
 
-		if(number > 30 || number < 0) {
-			swal(
-				'Error!',
-				'Number must be from 0-30!'
-			)
-		} else {
-			$('.grid table').show();
-			$('.grid table').html('');
+    if(number > 30 || number < 0) {
+      swal(
+        'Error!',
+        'Number must be from 0-30!'
+      )
+    } else {
+      $('.grid table').show();
+      $('.grid table').html('');
 
-			for (let i = 0; i < number; i++) {
-				$('.grid table').append('<tr></tr>');
-			}
+      for (let i = 0; i < number; i++) {
+        $('.grid table').append('<tr></tr>');
+      }
 
-			for (let y = 0; y < number; y++) {
-				$('.grid table tr').each(function() {
-					$(this).append('<td></td>');
+      for (let y = 0; y < number; y++) {
+        $('.grid table tr').each(function() {
+          $(this).append('<td></td>');
 
-					$('.grid table td').hover(function() {
-						let color = randomColor({
-							luminosity: 'light',
-   						hue: 'blue'
-						});
+          $('.grid table td').hover(function() {
+            let color = randomColor({
+              luminosity: 'light',
+              hue: 'blue'
+            });
 
-						$(this).css('background', color);
-					});
-				});
-			}
+            $(this).css('background', color);
+          });
+        });
+      }
 
-			$('form').hide();
-		}
-	});
+      $('form').hide();
+    }
+  });
 });
-
-
-
-
-
-
-
